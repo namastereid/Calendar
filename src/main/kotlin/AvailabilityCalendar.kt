a5f8c9b397a0da9719368de33a24c3e4fba3278d
Namaste Reid <namaste.reid@gmail.com>
2021-07-02 18:03:29 -0600

    Refactored to use ZonedDateTime to support time zones.

--- src/main/kotlin/AvailabilityCalendar.kt
+++ src/main/kotlin/AvailabilityCalendar.kt
@@ -2,29 +2,51 @@
 
 import com.google.common.collect.ImmutableRangeSet
 import com.google.common.collect.Range
-import com.google.common.collect.RangeSet
 import java.time.LocalDateTime
-import java.time.LocalTime
-import java.util.*
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
+/**
+ * [busyRangeSet] and [workingHours] are in local time. TimeZone will be used when [getAvailability] is called
+ * to match availability between calendars.
+ */
 data class AvailabilityCalendar(
-    val timezone: TimeZone,
-    val busyRangeSet: ImmutableRangeSet<LocalDateTime>,
-    val workingHours: Range<LocalTime>
+    val timeZone: ZoneId,
+    val busyRangeSet: ImmutableRangeSet<ZonedDateTime>,
+    val workingHours: Range<ZonedDateTime>
 ) {
+
+    /**
+     * Get [timeRange] with [zoneId] with the same instant.
+     */
+    private fun rangeWithZone(timeRange: Range<ZonedDateTime>, zoneId: ZoneId): Range<ZonedDateTime> {
+        return Range.range(
+            timeRange.lowerEndpoint().withZoneSameInstant(zoneId),
+            timeRange.lowerBoundType(),
+            timeRange.upperEndpoint().withZoneSameInstant(zoneId),
+            timeRange.upperBoundType()
+        )
+    }
 
     /**
      * Get the range of free times within the [timeRange].
      */
-    fun getFreeRangeSet(timeRange: Range<LocalDateTime>): ImmutableRangeSet<LocalDateTime> {
-        val start = timeRange.lowerEndpoint()
-        val end = timeRange.upperEndpoint()
+    fun getFreeRangeSet(timeRange: Range<ZonedDateTime>): ImmutableRangeSet<ZonedDateTime> {
+        val range = rangeWithZone(timeRange, timeZone)
+        val start = range.lowerEndpoint()
+        val end = range.upperEndpoint()
 
-        val builder = ImmutableRangeSet.Builder<LocalDateTime>()
-        for (d in start.toLocalDate().datesUntil(end.toLocalDate())) {
-            builder.add(Range.open(d.atTime(workingHours.lowerEndpoint()), d.atTime(workingHours.upperEndpoint())))
-        }
-        val workingRangeSet = builder.build();
+        val workingRangeSet =
+            start.toLocalDate().datesUntil(end.toLocalDate()).toList()
+                .fold(ImmutableRangeSet.Builder<ZonedDateTime>())
+                { builder, date ->
+                    builder.add(
+                        Range.open(
+                            date.atTime(workingHours.lowerEndpoint().toLocalTime()).atZone(timeZone),
+                            date.atTime(workingHours.upperEndpoint().toLocalTime()).atZone(timeZone)
+                        )
+                    )
+                }.build()
 
         return busyRangeSet.complement().subRangeSet(timeRange).intersection(workingRangeSet)
     }
@@ -34,11 +56,11 @@ data class AvailabilityCalendar(
      */
     fun getAvailability(
         calendars: List<AvailabilityCalendar>,
-        timeRange: Range<LocalDateTime>
-    ): ImmutableRangeSet<LocalDateTime> {
+        timeRange: Range<ZonedDateTime>
+    ): ImmutableRangeSet<ZonedDateTime> {
         //TODO handle timezones
         return calendars.fold(this.getFreeRangeSet(timeRange))
-        { acc: ImmutableRangeSet<LocalDateTime>, cal: AvailabilityCalendar ->
+        { acc: ImmutableRangeSet<ZonedDateTime>, cal: AvailabilityCalendar ->
             return acc.intersection(cal.getFreeRangeSet(timeRange))
         }
     }
